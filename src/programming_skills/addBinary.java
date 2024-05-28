930a161fff6d8001fe220f4119093cd5e6c19c86
jankozub <j.kozub136@gmail.com>
2024-05-28 16:51:12 +0200

    -daily leetcode

--- src/programming_skills/addBinary.java
+++ src/programming_skills/addBinary.java
@@ -10,32 +10,25 @@ public class addBinary {
         char num1, num2;
         int overflow = 0;
 
-        if (b.length() > a.length()) {
-            String c = a;
-            a = b;
-            b = c;
-        }
         char[] b1 = a.toCharArray();
         char[] b2 = b.toCharArray();
 
-        for (int i = 0; i < b1.length; i++) {
-            num1 = b1[b1.length - i - 1];
-
-            if (b2.length - i - 1 >= 0) num2 = b2[b2.length - i - 1];
-            else num2 = '0';
+        for (int i = 0; i < Math.max(b1.length, b2.length); i++) {
+            num1 = b1.length - i - 1 >= 0 ? b1[b1.length - i - 1] : '0';
+            num2 = b2.length - i - 1 >= 0 ? b2[b2.length - i - 1] : '0';
 
             if (num1 == '0' && num2 == '0') {
-                sb.insert(0, overflow == 0 ? '0' : '1');
+                sb.append(overflow == 0 ? '0' : '1');
                 overflow = 0;
             } else if ((num1 == '1' && num2 == '0') || (num1 == '0' && num2 == '1')) {
-                sb.insert(0, overflow == 0 ? '1' : '0');
+                sb.append(overflow == 0 ? '1' : '0');
             } else if (num1 == '1' && num2 == '1') {
-                sb.insert(0, overflow == 0 ? '0' : '1');
+                sb.append(overflow == 0 ? '0' : '1');
                 overflow = 1;
             }
         }
-        if (overflow == 1) sb.insert(0, '1');
+        if (overflow == 1) sb.append('1');
 
-        return sb.toString();
+        return sb.reverse().toString();
     }
 }
